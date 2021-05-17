9064422b4817cab13b97e50fc144ea03e3a1091f
Leonard Lau <17238@burnside.school.nz>
2021-05-18 11:41:55 +1200

    database table names updated, fixed typos in code, broken switch page added, needs fixing

--- routes.py
+++ routes.py
@@ -3,21 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABAASE_URI'] = 'sqlite:///switch.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///switch.db'
 db = SQLAlchemy(app)
 
-class Switches (db.Model):
-    __tablename__ = "Switches"
+class Switch (db.Model):
+    __tablename__ = "Switch"
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String())
     brand = db.Column(db.String())
     style = db.Column(db.String())
     color = db.Column(db.String())
-    spring = db.Column(db.String())
+    spring = db.Column(db.Integer())
     description = db.Column(db.String())
 
-class Prebuilts (db.Model):
-    __tablename__ = "Prebuilts"
+class Prebuilt (db.Model):
+    __tablename__ = "Prebuilt"
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String())
     layout = db.Column(db.String())
@@ -26,13 +26,21 @@ class Prebuilts (db.Model):
     rgb = db.Column(db.Boolean())
     description = db.Column(db.String())
 
+prebuilt = db.Table ('prebuilt', db.Column('sid', db.Integer, db.ForeignKey('Switch.id'), primary_key=True), db.Column('pid', db.Integer, db.ForeignKey('Prebuilt.id'), primary_key=True))
+
 @app.route('/')
 def home():
     return render_template("home.html", page_title="Home")
 
-@app.route('/prebuilts')
-def prebuilts():
-    return render_template("prebuilts.html", page_title="Prebuilts")
+@app.route('/switch/<int:id>')
+def switch(id):
+    switch = Switch.query.filter_by(id=id).first_or_404()
+    return render_template ("switch.html", page_title="Switches", switch = switch)
+
+#@app.route('/prebuilt/<int:id>')
+#def prebuilt(id):
+#    prebuilt = Prebuilt.query.filter_by(id=id).first_or_404()
+#    return render_template("prebuilts.html", page_title="Prebuilts", prebuilt = prebuilt)
 
 if __name__ == "__main__":
     app.run(port=3000)
