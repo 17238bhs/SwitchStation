4849f08401a0a60e10075c3215f3fbce92b1c36e
Leonard Lau <17238@burnside.school.nz>
2021-07-28 13:57:43 +1200

    all switches pages set up, link added to nav, more work on database done

--- routes.py
+++ routes.py
@@ -26,5 +26,15 @@ def switch(id):
     switch = models.Switch.query.filter_by(id=id).first_or_404()
     return render_template ("switch.html", page_title="Switches", switch = switch)
 
+@app.route('/all_prebuilts')
+def all_prebuilts():
+    results = models.Prebuilt.query.all()
+    return render_template ("all_prebuilts.html", page_title="All Prebuilts", prebuilts = results)
+
+@app.route('/prebuilt/<int:id>')
+def prebuilt(id):
+    prebuilt = models.Prebuilt.query.filter_by(id=id).first_or_404()
+    return render_template ("prebuilt.html", page_title="Prebuilts", prebuilt = prebuilt)
+
 if __name__ == "__main__":
     app.run(port=3000)
