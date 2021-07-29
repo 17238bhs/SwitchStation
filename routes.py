16565cf828a6ab4ceb875bcf885a8184c2059bc3
Leonard Lau <17238@burnside.school.nz>
2021-07-29 12:37:27 +1200

    comments added

--- routes.py
+++ routes.py
@@ -18,25 +18,25 @@ def learn():
 
 @app.route('/all_switches')
 def all_switches():
-    results = models.Switch.query.all()
+    results = models.Switch.query.all() #query for all switches
     return render_template ("all_switches.html", page_title="All Switches", switches = results)
 
 @app.route('/switch/<int:id>')
 def switch(id):
-    switch = models.Switch.query.filter_by(id=id).first_or_404()
+    switch = models.Switch.query.filter_by(id=id).first_or_404() #query for switch with specified id
     return render_template ("switch.html", page_title="Switches", switch = switch)
 
 @app.route('/all_prebuilts')
 def all_prebuilts():
-    results = models.Prebuilt.query.all()
+    results = models.Prebuilt.query.all() #query for all prebuilts
     return render_template ("all_prebuilts.html", page_title="All Prebuilts", prebuilts = results)
 
 @app.route('/prebuilt/<int:id>')
 def prebuilt(id):
-    prebuilt = models.Prebuilt.query.filter_by(id=id).first_or_404()
+    prebuilt = models.Prebuilt.query.filter_by(id=id).first_or_404() #query for prebuilt with specific id
     return render_template ("prebuilt.html", page_title="Prebuilts", prebuilt = prebuilt)
 
-@app.errorhandler(404)
+@app.errorhandler(404) #404 error handler, redirects to here when a 404 occurs
 def page_not_found(e):
     return render_template("404.html")
 
